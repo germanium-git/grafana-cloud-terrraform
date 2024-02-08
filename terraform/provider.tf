b2d622d5bbf812f2e82c69b88f6fb57fc909a966
Petr Nemec <petr.nemec@jamf.com>
2024-02-08 16:42:48 +0100

    initial commit

---
+++ terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        grafana = {
+            source = "grafana/grafana"
+            version = ">= 2.5.0"
+        }
+    }
+    backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "mygermanium"
+    workspaces {
+      name = "grafana-cloud"
+    }
+  }
+}
+
+provider "grafana" {
+  url  = var.GRAFANA_URL
+  auth = var.GRAFANA_AUTH
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "GRAFANA_URL" {
+  type        = string
+}
+
+variable "GRAFANA_AUTH" {
+  description = "Grafana API token"
+  type        = string
+}
