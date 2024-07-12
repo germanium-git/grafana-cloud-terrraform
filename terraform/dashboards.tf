3ea5a71661007c54c38f3e051169ba31ad8264fe
Petr Nemec <petr.nemec@jamf.com>
2024-07-12 12:33:56 +0200

    Teltonika LTE dashboard

--- terraform/dashboards.tf
+++ terraform/dashboards.tf
@@ -1744,3 +1744,590 @@ resource "grafana_dashboard" "traffic_statistics" {
     "weekStart" : ""
   })
 }
+
+resource "grafana_dashboard" "teltonika_lte" {
+  folder = grafana_folder.folder1.uid
+  config_json = jsonencode({
+    "annotations" : {
+      "list" : [
+        {
+          "builtIn" : 1,
+          "datasource" : {
+            "type" : "grafana",
+            "uid" : "-- Grafana --"
+          },
+          "enable" : true,
+          "hide" : true,
+          "iconColor" : "rgba(0, 211, 255, 1)",
+          "name" : "Annotations & Alerts",
+          "type" : "dashboard"
+        }
+      ]
+    },
+    "editable" : true,
+    "fiscalYearStartMonth" : 0,
+    "graphTooltip" : 0,
+    "id" : 24,
+    "links" : [],
+    "liveNow" : false,
+    "panels" : [
+      {
+        "datasource" : {
+          "type" : "influxdb",
+          "uid" : "11e7VrHnk"
+        },
+        "description" : "SINR (The signal-to-noise ratio) value in dB.\n* more than 21 dB - very good\n* from 13 dB to 20 dB - good\n* from 0 dB do 12 dB - bad, an external antenna is needed.\n* less than 0 dB – very poor conditions, an external antenna is needed.",
+        "fieldConfig" : {
+          "defaults" : {
+            "color" : {
+              "mode" : "palette-classic"
+            },
+            "custom" : {
+              "axisBorderShow" : false,
+              "axisCenteredZero" : false,
+              "axisColorMode" : "text",
+              "axisLabel" : "",
+              "axisPlacement" : "auto",
+              "barAlignment" : 0,
+              "barWidthFactor" : 0.6,
+              "drawStyle" : "line",
+              "fillOpacity" : 0,
+              "gradientMode" : "none",
+              "hideFrom" : {
+                "legend" : false,
+                "tooltip" : false,
+                "viz" : false
+              },
+              "insertNulls" : false,
+              "lineInterpolation" : "linear",
+              "lineWidth" : 1,
+              "pointSize" : 5,
+              "scaleDistribution" : {
+                "type" : "linear"
+              },
+              "showPoints" : "auto",
+              "spanNulls" : false,
+              "stacking" : {
+                "group" : "A",
+                "mode" : "none"
+              },
+              "thresholdsStyle" : {
+                "mode" : "off"
+              }
+            },
+            "mappings" : [],
+            "thresholds" : {
+              "mode" : "absolute",
+              "steps" : [
+                {
+                  "color" : "green",
+                  "value" : null
+                },
+                {
+                  "color" : "red",
+                  "value" : 80
+                }
+              ]
+            }
+          },
+          "overrides" : []
+        },
+        "gridPos" : {
+          "h" : 8,
+          "w" : 12,
+          "x" : 0,
+          "y" : 0
+        },
+        "id" : 1,
+        "options" : {
+          "legend" : {
+            "calcs" : [],
+            "displayMode" : "list",
+            "placement" : "bottom",
+            "showLegend" : true
+          },
+          "tooltip" : {
+            "mode" : "single",
+            "sort" : "none"
+          }
+        },
+        "targets" : [
+          {
+            "datasource" : {
+              "type" : "influxdb",
+              "uid" : "11e7VrHnk"
+            },
+            "query" : "from(bucket: \"Network\")\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n  |> filter(fn: (r) => r[\"_field\"] == \"mSINR\")\n  |> aggregateWindow(every: v.windowPeriod, fn: last, createEmpty: false)\n  |> yield(name: \"last\")",
+            "refId" : "A"
+          }
+        ],
+        "title" : "mSINR",
+        "type" : "timeseries"
+      },
+      {
+        "datasource" : {
+          "type" : "influxdb",
+          "uid" : "11e7VrHnk"
+        },
+        "description" : "Signal strength level",
+        "fieldConfig" : {
+          "defaults" : {
+            "color" : {
+              "mode" : "palette-classic"
+            },
+            "custom" : {
+              "axisBorderShow" : false,
+              "axisCenteredZero" : false,
+              "axisColorMode" : "text",
+              "axisLabel" : "",
+              "axisPlacement" : "auto",
+              "barAlignment" : 0,
+              "barWidthFactor" : 0.6,
+              "drawStyle" : "line",
+              "fillOpacity" : 0,
+              "gradientMode" : "none",
+              "hideFrom" : {
+                "legend" : false,
+                "tooltip" : false,
+                "viz" : false
+              },
+              "insertNulls" : false,
+              "lineInterpolation" : "linear",
+              "lineWidth" : 1,
+              "pointSize" : 5,
+              "scaleDistribution" : {
+                "type" : "linear"
+              },
+              "showPoints" : "auto",
+              "spanNulls" : false,
+              "stacking" : {
+                "group" : "A",
+                "mode" : "none"
+              },
+              "thresholdsStyle" : {
+                "mode" : "off"
+              }
+            },
+            "mappings" : [],
+            "thresholds" : {
+              "mode" : "absolute",
+              "steps" : [
+                {
+                  "color" : "green",
+                  "value" : null
+                },
+                {
+                  "color" : "red",
+                  "value" : 80
+                }
+              ]
+            }
+          },
+          "overrides" : []
+        },
+        "gridPos" : {
+          "h" : 8,
+          "w" : 12,
+          "x" : 12,
+          "y" : 0
+        },
+        "id" : 2,
+        "options" : {
+          "legend" : {
+            "calcs" : [],
+            "displayMode" : "list",
+            "placement" : "bottom",
+            "showLegend" : true
+          },
+          "tooltip" : {
+            "mode" : "single",
+            "sort" : "none"
+          }
+        },
+        "targets" : [
+          {
+            "datasource" : {
+              "type" : "influxdb",
+              "uid" : "11e7VrHnk"
+            },
+            "query" : "from(bucket: \"Network\")\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n  |> filter(fn: (r) => r[\"_field\"] == \"mSignal\")\n  |> aggregateWindow(every: v.windowPeriod, fn: last, createEmpty: false)\n  |> yield(name: \"last\")",
+            "refId" : "A"
+          }
+        ],
+        "title" : "mSignal",
+        "type" : "timeseries"
+      },
+      {
+        "datasource" : {
+          "type" : "influxdb",
+          "uid" : "11e7VrHnk"
+        },
+        "description" : "RSRP (Reference Signal Receive Power) value in dBm\n* more than -79 dBm - very good signal\n* from -80 dBm to -90 dBm - good signal\n* from -91 dBm to -100 dBm - weak signal, an external antenna is recommended or a better position of the modem is needed.\n* less than -100 dBm - very poor signal, an external antenna is needed.",
+        "fieldConfig" : {
+          "defaults" : {
+            "color" : {
+              "mode" : "palette-classic"
+            },
+            "custom" : {
+              "axisBorderShow" : false,
+              "axisCenteredZero" : false,
+              "axisColorMode" : "text",
+              "axisLabel" : "",
+              "axisPlacement" : "auto",
+              "barAlignment" : 0,
+              "barWidthFactor" : 0.6,
+              "drawStyle" : "line",
+              "fillOpacity" : 0,
+              "gradientMode" : "none",
+              "hideFrom" : {
+                "legend" : false,
+                "tooltip" : false,
+                "viz" : false
+              },
+              "insertNulls" : false,
+              "lineInterpolation" : "linear",
+              "lineWidth" : 1,
+              "pointSize" : 5,
+              "scaleDistribution" : {
+                "type" : "linear"
+              },
+              "showPoints" : "auto",
+              "spanNulls" : false,
+              "stacking" : {
+                "group" : "A",
+                "mode" : "none"
+              },
+              "thresholdsStyle" : {
+                "mode" : "off"
+              }
+            },
+            "mappings" : [],
+            "max" : -70,
+            "min" : -110,
+            "thresholds" : {
+              "mode" : "absolute",
+              "steps" : [
+                {
+                  "color" : "green",
+                  "value" : null
+                },
+                {
+                  "color" : "red",
+                  "value" : 80
+                }
+              ]
+            }
+          },
+          "overrides" : []
+        },
+        "gridPos" : {
+          "h" : 8,
+          "w" : 12,
+          "x" : 0,
+          "y" : 8
+        },
+        "id" : 4,
+        "options" : {
+          "legend" : {
+            "calcs" : [],
+            "displayMode" : "list",
+            "placement" : "bottom",
+            "showLegend" : true
+          },
+          "tooltip" : {
+            "mode" : "single",
+            "sort" : "none"
+          }
+        },
+        "targets" : [
+          {
+            "datasource" : {
+              "type" : "influxdb",
+              "uid" : "11e7VrHnk"
+            },
+            "query" : "from(bucket: \"Network\")\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n  |> filter(fn: (r) => r[\"_field\"] == \"mRSRP\")\n  |> aggregateWindow(every: v.windowPeriod, fn: last, createEmpty: false)\n  |> yield(name: \"last\")",
+            "refId" : "A"
+          }
+        ],
+        "title" : "mRSRP",
+        "type" : "timeseries"
+      },
+      {
+        "datasource" : {
+          "type" : "influxdb",
+          "uid" : "11e7VrHnk"
+        },
+        "description" : "RSRQ (Reference Signal Receive Quality) value in dB\nRSRQ – Indicates quality of the received signal, and its range is typically -19.5dB(bad) to -3dB (good).\n* more than -9 dB - very good\n* from -10 dB to -15 dB - good\n* form -16 dB to -20 dB - bad, an external antenna is needed or a better position of the modem.\n* less than -20 dB – very bad, an external antenna is needed.",
+        "fieldConfig" : {
+          "defaults" : {
+            "color" : {
+              "mode" : "palette-classic"
+            },
+            "custom" : {
+              "axisBorderShow" : false,
+              "axisCenteredZero" : false,
+              "axisColorMode" : "text",
+              "axisLabel" : "",
+              "axisPlacement" : "auto",
+              "barAlignment" : 0,
+              "barWidthFactor" : 0.6,
+              "drawStyle" : "line",
+              "fillOpacity" : 0,
+              "gradientMode" : "none",
+              "hideFrom" : {
+                "legend" : false,
+                "tooltip" : false,
+                "viz" : false
+              },
+              "insertNulls" : false,
+              "lineInterpolation" : "linear",
+              "lineWidth" : 1,
+              "pointSize" : 5,
+              "scaleDistribution" : {
+                "type" : "linear"
+              },
+              "showPoints" : "auto",
+              "spanNulls" : false,
+              "stacking" : {
+                "group" : "A",
+                "mode" : "none"
+              },
+              "thresholdsStyle" : {
+                "mode" : "off"
+              }
+            },
+            "mappings" : [],
+            "thresholds" : {
+              "mode" : "absolute",
+              "steps" : [
+                {
+                  "color" : "green",
+                  "value" : null
+                },
+                {
+                  "color" : "red",
+                  "value" : 80
+                }
+              ]
+            }
+          },
+          "overrides" : []
+        },
+        "gridPos" : {
+          "h" : 8,
+          "w" : 12,
+          "x" : 12,
+          "y" : 8
+        },
+        "id" : 5,
+        "options" : {
+          "legend" : {
+            "calcs" : [],
+            "displayMode" : "list",
+            "placement" : "bottom",
+            "showLegend" : true
+          },
+          "tooltip" : {
+            "mode" : "single",
+            "sort" : "none"
+          }
+        },
+        "targets" : [
+          {
+            "datasource" : {
+              "type" : "influxdb",
+              "uid" : "11e7VrHnk"
+            },
+            "query" : "from(bucket: \"Network\")\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n  |> filter(fn: (r) => r[\"_field\"] == \"mRSRQ\")\n  |> aggregateWindow(every: v.windowPeriod, fn: last, createEmpty: false)\n  |> yield(name: \"last\")",
+            "refId" : "A"
+          }
+        ],
+        "title" : "mRSRQ",
+        "type" : "timeseries"
+      },
+      {
+        "datasource" : {
+          "type" : "influxdb",
+          "uid" : "11e7VrHnk"
+        },
+        "description" : "Cell ID",
+        "fieldConfig" : {
+          "defaults" : {
+            "color" : {
+              "mode" : "thresholds"
+            },
+            "custom" : {
+              "fillOpacity" : 35,
+              "hideFrom" : {
+                "legend" : false,
+                "tooltip" : false,
+                "viz" : false
+              },
+              "insertNulls" : false,
+              "lineWidth" : 1,
+              "spanNulls" : false
+            },
+            "displayName" : "CellID",
+            "mappings" : [],
+            "thresholds" : {
+              "mode" : "absolute",
+              "steps" : [
+                {
+                  "color" : "green",
+                  "value" : null
+                },
+                {
+                  "color" : "red",
+                  "value" : 80
+                }
+              ]
+            }
+          },
+          "overrides" : []
+        },
+        "gridPos" : {
+          "h" : 7,
+          "w" : 24,
+          "x" : 0,
+          "y" : 16
+        },
+        "id" : 6,
+        "options" : {
+          "alignValue" : "center",
+          "legend" : {
+            "displayMode" : "table",
+            "placement" : "bottom",
+            "showLegend" : false
+          },
+          "mergeValues" : true,
+          "rowHeight" : 0.89,
+          "showValue" : "auto",
+          "tooltip" : {
+            "mode" : "none",
+            "sort" : "none"
+          }
+        },
+        "pluginVersion" : "10.2.0-60139",
+        "targets" : [
+          {
+            "datasource" : {
+              "type" : "influxdb",
+              "uid" : "11e7VrHnk"
+            },
+            "query" : "from(bucket: \"Network\")\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n  |> filter(fn: (r) => r[\"_field\"] == \"mCellID\")\n  |> aggregateWindow(every: v.windowPeriod, fn: last, createEmpty: false)\n  |> yield(name: \"last\")",
+            "refId" : "A"
+          }
+        ],
+        "title" : "mCellID",
+        "type" : "state-timeline"
+      },
+      {
+        "datasource" : {
+          "type" : "influxdb",
+          "uid" : "11e7VrHnk"
+        },
+        "description" : "Modem's temperature in 0.1 degrees Celsius",
+        "fieldConfig" : {
+          "defaults" : {
+            "color" : {
+              "mode" : "palette-classic"
+            },
+            "custom" : {
+              "axisCenteredZero" : false,
+              "axisColorMode" : "text",
+              "axisLabel" : "",
+              "axisPlacement" : "auto",
+              "axisShow" : false,
+              "barAlignment" : 0,
+              "drawStyle" : "line",
+              "fillOpacity" : 0,
+              "gradientMode" : "none",
+              "hideFrom" : {
+                "legend" : false,
+                "tooltip" : false,
+                "viz" : false
+              },
+              "insertNulls" : false,
+              "lineInterpolation" : "linear",
+              "lineWidth" : 1,
+              "pointSize" : 5,
+              "scaleDistribution" : {
+                "type" : "linear"
+              },
+              "showPoints" : "auto",
+              "spanNulls" : false,
+              "stacking" : {
+                "group" : "A",
+                "mode" : "none"
+              },
+              "thresholdsStyle" : {
+                "mode" : "off"
+              }
+            },
+            "mappings" : [],
+            "thresholds" : {
+              "mode" : "absolute",
+              "steps" : [
+                {
+                  "color" : "green"
+                },
+                {
+                  "color" : "red",
+                  "value" : 80
+                }
+              ]
+            }
+          },
+          "overrides" : []
+        },
+        "gridPos" : {
+          "h" : 8,
+          "w" : 5,
+          "x" : 0,
+          "y" : 23
+        },
+        "id" : 3,
+        "options" : {
+          "legend" : {
+            "calcs" : [],
+            "displayMode" : "list",
+            "placement" : "bottom",
+            "showLegend" : true
+          },
+          "tooltip" : {
+            "mode" : "single",
+            "sort" : "none"
+          }
+        },
+        "targets" : [
+          {
+            "datasource" : {
+              "type" : "influxdb",
+              "uid" : "11e7VrHnk"
+            },
+            "query" : "from(bucket: \"Network\")\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\n  |> filter(fn: (r) => r[\"_field\"] == \"mTemperature\")\n  |> aggregateWindow(every: v.windowPeriod, fn: last, createEmpty: false)\n  |> yield(name: \"last\")",
+            "refId" : "A"
+          }
+        ],
+        "title" : "mTemperature",
+        "type" : "timeseries"
+      }
+    ],
+    "refresh" : "",
+    "schemaVersion" : 39,
+    "tags" : [],
+    "templating" : {
+      "list" : []
+    },
+    "time" : {
+      "from" : "now-6h",
+      "to" : "now"
+    },
+    "timepicker" : {},
+    "timezone" : "",
+    "title" : "Teltonika LTE",
+    "uid" : "teltonika-lte",
+    "version" : 1,
+    "weekStart" : ""
+  })
+}
